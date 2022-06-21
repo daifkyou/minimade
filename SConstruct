daifkyou/minimade

commit 73973a18ffdd9eb23f968aa4f57d02ee7f28f9c7

font shenanigans

SConstruct
@@ -24,6 +24,11 @@ AddOption('--no-std', '--no-standard',
           action='store_true',
           help="Don\'t build osu!standard elements")
 
+AddOption('--no-tk', '--no-taiko',
+          dest='no_taiko',
+          action='store_true',
+          help="Don\'t build osu!taiko elements")
+
 AddOption('--aspect-ratio',
           dest='aspect_ratio',
           type='string',
@@ -378,14 +383,17 @@ GLYPH_WIDTH_OFFSET = {
 }
 
 
-def font(font_name, glyphs, scale=20, alignx='left', aligny='top'):
+OVERLAP = -6 # we draw overlap into the skin instead of using skin.ini because for some reason ranking screen doesnt respect it
+
+
+def font(font_name, glyphs, scale=20, aligny='top'):
     """render font"""
     glyphs = map(lambda g: (str(g), (CHAR_REPLACE[glyph] if (
         glyph := str(g)) in CHAR_REPLACE else glyph)), glyphs)
 
-    def get_render_font_glyph_1x(glyph, scale):
+    def get_render_font_glyph(glyph, scale, width_override = None):
         # cairo text actually sucks im just going to commit this shit i give up
-        def render_font_glyph_1x(target, source, env):
+        def render_font_glyph(target, source, env):
             surface = cairocffi.ImageSurface(
                 cairocffi.FORMAT_ARGB32, 16 * scale, 16 * scale)
             ctx = cairocffi.Context(surface)
@@ -395,18 +403,18 @@ def font(font_name, glyphs, scale=20, alignx='left', aligny='top'):
             ctx.select_font_face('osifont')
             ctx.set_font_size(1)
             ascent, descent, _, _, _ = ctx.font_extents()
-            text_x_bearing, _, text_width, _, text_x_advance, _ = ctx.text_extents(
+            text_x_bearing, _, text_width, _, _, _ = ctx.text_extents(
                 glyph)
 
-            if(alignx == 'middle'):
-                x = -text_x_bearing
-                width = text_width
-            elif(alignx == 'left'):
-                x = 0
-                width = text_x_advance
+            if(width_override != None): width = width_override
+            else: width = text_width
+
+            x = -text_x_bearing
 
             if(glyph in GLYPH_WIDTH_OFFSET):
                 width += GLYPH_WIDTH_OFFSET[glyph]
+
+            width -= OVERLAP / scale
 
             if(aligny == 'middle'):
                 height = ascent + 2 * descent
@@ -433,31 +441,30 @@ def font(font_name, glyphs, scale=20, alignx='left', aligny='top'):
 
             cropped_surface.write_to_png(str(target[0]))
 
-        return render_font_glyph_1x
-
-    def get_render_font_glyph_2x(glyph, scale):
-        return get_render_font_glyph_1x(glyph, scale * 2)
+        return render_font_glyph
 
     for glyph_name, glyph in glyphs:
+        width_override = 0.4 if glyph in [str(n) for n in range(10)] else None
+
         if not GetOption('no_1x'):
             env.Command(
                 '$BUILDDIR/' + font_name + '-' + glyph_name + '.png',
                 [],
-                action=get_render_font_glyph_1x(glyph, scale))
+                action=get_render_font_glyph(glyph, scale, width_override))
 
         if not GetOption('no_2x'):
             env.Command(
                 '$BUILDDIR/' + font_name + '-' + glyph_name + '@2x.png',
                 [],
-                action=get_render_font_glyph_2x(glyph, scale))
+                action=get_render_font_glyph(glyph, scale * 2, width_override))
 
 
-font('default', range(10), 35, 'middle', 'middle')
-font('score', [*range(10), 'comma', 'dot'], 40, 'left', 'middle')
+font('default', range(10), 35, 'middle')
+font('score', [*range(10), 'comma', 'dot'], 40, 'middle')
 env.Empty('score-x')
 env.Empty('score-percent')
 font('scoreentry', [*range(10), 'comma', 'dot',
-     'percent', 'x'], 15, 'left', 'middle')
+     'percent', 'x'], 15, 'middle')
 
 # masking border
 env.Empty('masking-border')

src/graphics/gameplay/osu/spinner/rpm.svg
@@ -5,8 +5,8 @@
    height="56"
    viewBox="0 0 280 56"
    id="svg4"
-   sodipodi:docname="spinner-rpm.svg"
-   inkscape:version="1.1.1 (3bf5ae0d25, 2021-09-20, custom)"
+   sodipodi:docname="rpm.svg"
+   inkscape:version="1.2 (dc2aedaf03, 2022-05-15)"
    xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
    xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
    xmlns="http://www.w3.org/2000/svg"
@@ -26,10 +26,10 @@
      inkscape:bbox-paths="true"
      inkscape:snap-page="true"
      inkscape:zoom="2.6479676"
-     inkscape:cx="102.72029"
-     inkscape:cy="49.471904"
+     inkscape:cx="145.77218"
+     inkscape:cy="41.35247"
      inkscape:window-width="1920"
-     inkscape:window-height="1007"
+     inkscape:window-height="1003"
      inkscape:window-x="0"
      inkscape:window-y="0"
      inkscape:window-maximized="1"
@@ -41,16 +41,18 @@
      inkscape:bbox-nodes="true"
      inkscape:snap-object-midpoints="true"
      inkscape:snap-bbox-edge-midpoints="true"
-     inkscape:snap-bbox-midpoints="true" />
+     inkscape:snap-bbox-midpoints="true"
+     inkscape:showpageshadow="0"
+     inkscape:deskcolor="#505050" />
   <text
      xml:space="preserve"
-     style="font-size:56.9642px;line-height:1.25;font-family:osifont;-inkscape-font-specification:osifont;fill:#ffffff;fill-opacity:1;stroke-width:0.999994"
-     x="-12.766878"
-     y="51.999969"
+     style="font-size:36px;line-height:1.25;font-family:osifont;-inkscape-font-specification:osifont;fill:#ffffff;fill-opacity:1;stroke-width:0.999994"
+     x="111.93164"
+     y="44.542969"
      id="text4193"><tspan
        sodipodi:role="line"
        id="tspan4191"
-       x="-12.766878"
-       y="51.999969"
-       style="stroke-width:0.999994">RPM</tspan></text>
+       x="111.93164"
+       y="44.542969"
+       style="font-size:36px;stroke-width:0.999994">RPM</tspan></text>
 </svg>

src/meta/skin.ini
@@ -21,10 +21,3 @@ SliderBorder: 255,255,255
 SliderTrackOverride: 0,0,0
 SpinnerBackground: 255,255,255
 MenuGlow: 255,255,255
-
-[Fonts]
-HitCirclePrefix: default
-ScorePrefix: score
-HitCircleOverlap: -2
-ScoreOverlap: -2
-ComboOverlap: -2

commit 7b5846107cf052ec640e54cce81050b0f11a633f

fix broken alignments caused by 73973a18ffdd9eb23f968aa4f57d02ee7f28f9c7

SConstruct
@@ -371,7 +371,7 @@ env.Empty('ranking-accuracy')
 
 
 # fonts
-CHAR_REPLACE = {
+CHAR_REPLACE = { # special characters in filenames and their corresponding characters
     'comma': ',',
     'dot': '.',
     'percent': '%'
@@ -386,7 +386,7 @@ GLYPH_WIDTH_OFFSET = {
 OVERLAP = -6 # we draw overlap into the skin instead of using skin.ini because for some reason ranking screen doesnt respect it
 
 
-def font(font_name, glyphs, scale=20, aligny='top'):
+def font(font_name, glyphs, scale=20, alignx='left', aligny='top'):
     """render font"""
     glyphs = map(lambda g: (str(g), (CHAR_REPLACE[glyph] if (
         glyph := str(g)) in CHAR_REPLACE else glyph)), glyphs)
@@ -406,15 +406,20 @@ def font(font_name, glyphs, scale=20, aligny='top'):
             text_x_bearing, _, text_width, _, _, _ = ctx.text_extents(
                 glyph)
 
-            if(width_override != None): width = width_override
-            else: width = text_width
+            if alignx == 'left':
+                if width_override != None:
+                    width = width_override
+                else:
+                    x = 0
+                    width = text_x_advance
+            elif alignx == 'middle':
+                x = -text_x_bearing - OVERLAP / scale / 2
+                width = text_width
 
-            x = -text_x_bearing
+            width -= OVERLAP / scale
 
             if(glyph in GLYPH_WIDTH_OFFSET):
                 width += GLYPH_WIDTH_OFFSET[glyph]
-
-            width -= OVERLAP / scale
 
             if(aligny == 'middle'):
                 height = ascent + 2 * descent
@@ -459,19 +464,18 @@ def font(font_name, glyphs, scale=20, aligny='top'):
                 action=get_render_font_glyph(glyph, scale * 2, width_override))
 
 
-font('default', range(10), 35, 'middle')
+font('default', range(10), 35, 'middle', 'middle')
 font('score', [*range(10), 'comma', 'dot'], 40, 'middle')
 env.Empty('score-x')
 env.Empty('score-percent')
 font('scoreentry', [*range(10), 'comma', 'dot',
-     'percent', 'x'], 15, 'middle')
+     'percent', 'x'], 15, 'middle', 'middle')
 
 # masking border
 env.Empty('masking-border')
 
 
 # scorebar (surprisingly)
-
 ADDED_SCOREBAR = True
 render_default('scorebar-bg', 'graphics/interface/hud/scorebar/background')
 render_default('scorebar-colour', 'graphics/interface/hud/scorebar/colour')